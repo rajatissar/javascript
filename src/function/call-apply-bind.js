a519f762e4f1b10753bbc4f52449bd3a1a2de18e
Rajat <rajatissar94@gmail.com>
2021-03-03 11:01:15 +0530

    call apply updated

--- src/function/call-apply-bind.js
+++ src/function/call-apply-bind.js
@@ -1,6 +1,3 @@
-// Global Scope
-console.log('Global Scope this', this);
-
 const person1 = {
     firstName: 'Samrat',
     lastName: 'Sharma',
@@ -18,7 +15,12 @@ const fullName2 = function (hometown, state) {
 
 
 // -------------------------------------------------------------------------
-// call (function borrowing) (invoke function)
+// call
+// function borrowing / method reuse
+// It invokes function
+// With the call() method, you can write a method that can be used on different objects.
+// In JavaScript all functions are object methods.
+// If a function is not a method of a JavaScript object, it is a function of the global object
 const nameOfPerson = person1.fullName.call(person2, 'Ambala', 'Haryana');
 console.log('nameOfPerson1', nameOfPerson);
 const nameOfPerson2 = fullName2.call(person1, 'Mohali', 'Punjab');
@@ -26,7 +28,11 @@ console.log('nameOfPerson2', nameOfPerson2);
 
 
 // -------------------------------------------------------------------------
-// apply (function borrowing) (invoke function)
+// apply 
+// function borrowing
+// invoke function
+// The call() method takes arguments separately.
+// The apply() method takes arguments as an array.
 const nameOfPerson3 = person1.fullName.apply(person2, ['Ambala', 'Haryana']);
 console.log('nameOfPerson3', nameOfPerson);
 const nameOfPerson4 = fullName2.apply(person1, ['Mohali', 'Punjab']);
